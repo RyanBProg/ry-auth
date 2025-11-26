2cc7206360fa90c18d3c0a31771dfd7cfdd14eba
Ryan <ryanbprog@gmail.com>
2025-11-26 20:11:07 +0800

    biome added to project

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const login = (email: string, password: string) => "implement me";
+const register = (email: string, password: string) => "implement me";
+const logout = () => "implement me";
+const isAuth = (token: string) => "implement me";
+const refreshToken = (token: string) => "implement me";
+
+export { login, register, logout, isAuth, refreshToken };
